670fbe07727ee545ca08fa3cdcca7bdb6c479ca5
magicFeirl <2100709458@qq.com>
2024-04-19 17:31:35 +0800

    feat: add config and docs

---
+++ config.example.py
@@ -0,0 +1,38 @@
+USERS = [
+    # user 1
+    {
+        'NAME': 'user1',
+        # your uid
+        'UID': '2',
+        # your sessdata & bili_jct (get them from cookie)
+        'SESSDATA': '',
+        'BILI_JCT': '',
+        # only checkout user where in UID_WHITE_LIST
+        'UID_WHITE_LIST': [333]
+    },
+    # user 2
+    {
+        'NAME': 'user1',
+        # your uid
+        'UID': '3',
+        # your sessdata & bili_jct (get them from cookie)
+        'SESSDATA': '',
+        'BILI_JCT': ''
+        # no uid white list so skip this user.
+    },
+    # ...
+]
+
+# Danmaku message will be randomly choiced from MSG_LIST
+MSG_LIST = [
+    "ヾ(＾-＾)ノ",
+    "٩(◕‿◕)۶",
+    "（╹◡╹）",
+    "⊂(◉‿◉)つ",
+    "(*^▽^*)",
+    "٩(^ᴗ^)۶",
+    "ฅ^•ﻌ•^ฅ",
+    "o(〃＾▽＾〃)o",
+    "(≧◡≦)",
+    "ପ(⑅ ॣ•͈ᴗ•͈ ॣ)∗˚"
+]
